a4008479de2d97e60abebbf10b4da00fb5778ca7
conpliance2 <dxdgamjt@gmail.com>
2017-09-14 19:57:27 +0900

    HTML、CSS編集

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.default')
 @section('content')
 <div class="wrapper">
-  <form action="{{ url('/login/conf2') }}" method="post" class="passform">
+	<div class="m-icon">
+		<p><i class="fa fa-envelope fa-big" aria-hidden="true"></i></p>
+	</div>
+  <form class="loginform" action="{{ url('/login/conf2') }}" method="post" class="passform">
     <h2 class="pass">メールアドレス</h2>
     {{ csrf_field() }}
     <input class="psps" type="email" name="email" required>
     <input class="psbtn" type="submit" value="ログイン" >
   </form>
+  <a class="logintop" href="http://caching-atm.net/public/login/signup">トップに戻る</a>
 </div>
 @endsection
