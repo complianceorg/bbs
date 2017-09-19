c78ecc603e514b62482dd6b572c3a4644e5decc3
conpliance2 <dxdgamjt@gmail.com>
2017-09-19 19:37:45 +0900

    CSSとHTML編集

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -16,11 +16,11 @@
     </ul>
   </div>
   <div class="keijiban">
-    <h2>新しいスレッドを立てる</h2>
+      <h2>ー  新しいスレッドを立てる  ー</h2>
     <form action= {{ url('/posts') }} method="post">
       {{csrf_field()}}
     <label for="" class="k-name">
-      <span>カテゴリ</span>
+      <span>カテゴリ:</span>
       <select name="cat_id">
          @foreach($categories as $category)
         <option value="{{ $category->name}}">{{ $category->name }}</option>
@@ -28,32 +28,38 @@
       </select>
     </label>
     <label for="" class="">
-      <span>タイトル</span>
-      <input type="text" name="title" required>
+      <span>タイトル:</span>
+      <input type="text" name="title" size="40" required>
     </label>
     <label for="" class="k-text">
-      <span>内容</span>
-      <textarea name="content" id="" cols="60" rows="10" required></textarea>
+      <span>内容:</span>
+      <textarea name="content" id="" cols="110" rows="10" required></textarea>
     </label>
       <input type="submit">
     </form>
   </div>
   <div class="detail">
-    <h2>既存のスレッドを見る</h2>
+    <h2>ー  既存のスレッド一覧  ー</h2>
 <?php foreach ($existcat as $key => $category) {
         echo <<< EOM
+        <h3>◆カテゴリー：{$category['cat_id']}</h3>
+        <div class="detail-box2">
         <div class="detail-box" id="{$category['cat_id']}">
-        <h3>カテゴリー：{$category['cat_id']}</h3>
+
 EOM;
       foreach ($posts["{$category['cat_id']}"] as $key => $post) {
         echo <<< EOM
+        <div class="detail-wrap">
         <p class="detail-number">
-        <span>タイトル：{$post['title']}</span>
+        <span>■タイトル：{$post['title']}</span>
         </p>
-        <p>内容：{$post['content']}</p>
-        <a href="./posts/single/{$post['id']}">続きを読む</a>
+        <p class="detail-content">内容：{$post['content']}</p>
+        <a href="./posts/single/{$post['id']}">このスレッドを読む</a>
+        </div>
+
 EOM;
 }
+echo "</div>";
 echo "</div>";
 }
 ?>
